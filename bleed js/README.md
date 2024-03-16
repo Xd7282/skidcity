8355a8c2605c261ac33e7495f7b1cb3d4b565a24
Xd7282 <127481404+Xd7282@users.noreply.github.com>
2024-03-16 15:00:32 +0530

    Delete bleed js directory

--- bleed js/README.md
+++
@@ -1,3 +0,0 @@
-# BLEED JS
-Literally just the JS version of bleed
-> Don't know the original poster.
--- bleed js/events/ready.js
+++
@@ -1,9 +0,0 @@
-const client = require('../bleed')
-
-client.on("ready", () => {
-  console.log(`${client.user.username} is now up and running!`);
-  client.user.setActivity(`discord.gg/four`, {
-    type: "PLAYING",
-    url: "https://www.twitch.tv/discord"
-  })
-})
