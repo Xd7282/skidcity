5e2588730d5b6a7ca7f5d751c45b1eea0d7cbf41
Xd7282 <127481404+Xd7282@users.noreply.github.com>
2024-03-16 14:47:57 +0530

    Delete slatt directory

--- slatt/README.md
+++
@@ -1,3 +0,0 @@
-# SLATT
-A Discord bot made by **conspiracy**
-> leaked by (`rx#1337`) and caden (`maserati#1337`)
--- slatt/src/events/error.js
+++
@@ -1,8 +0,0 @@
-module.exports = (client, error) => {
-  if (err instanceof DiscordAPIError) {
-    let stack = err.stack.split('at')
-    client.logger.error(`${stack[0]}`)
-  } else {
-    client.logger.error(err)
-  }
-};
